5ebab1e7c24ad7147bbf15119c35cb37cf4c4739
kleinalex-01 <kleinalex215@gmail.com>
2025-08-12 13:02:46 +0200

    Viewport fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,6 +9,23 @@ const Home: React.FC = () => {
     '/led-treatment/genoled-3.png'
   ]
 
+  // Fix mobile viewport height issue
+  useEffect(() => {
+    const setVH = () => {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+    
+    setVH()
+    window.addEventListener('resize', setVH)
+    window.addEventListener('orientationchange', setVH)
+    
+    return () => {
+      window.removeEventListener('resize', setVH)
+      window.removeEventListener('orientationchange', setVH)
+    }
+  }, [])
+
   // Auto-slide LED images every 4 seconds
   useEffect(() => {
     const interval = setInterval(() => {
